402979e7590d4cc2d468ce24deaf02090e1530ed
Elimar Macena <elimar.macena@gmail.com>
2019-11-26 22:57:16 -0300

    Update BaseCon.py

--- Model/BaseCon.py
+++ Model/BaseCon.py
@@ -3,9 +3,4 @@ from peewee import *
 
 class BaseCon(Model):
     class Meta:
-        database = None
-        import urlparse, psycopg2
-        urlparse.uses_netloc.append('postgres')
-        url = urlparse.urlparse(os.environ["postgres://eqvhegcr:uOr1RvzMuCvjqpnSPqvadvOnBwZP5oNd@motty.db.elephantsql.com:5432/eqvhegcr"])
-        database = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
-
+        database = PostgresqlDatabase(database='eqvhegcr', user='eqvhegcr', password='uOr1RvzMuCvjqpnSPqvadvOnBwZP5oNd', host='motty.db.elephantsql.com', port='5432')
