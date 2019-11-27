3079f47bebd518b1c88c0627185488bf24dd477c
Elimar Macena <elimar.macena@gmail.com>
2019-11-26 22:49:32 -0300

    Update BaseCon.py

--- Model/BaseCon.py
+++ Model/BaseCon.py
@@ -4,10 +4,8 @@ from peewee import *
 class BaseCon(Model):
     class Meta:
         database = None
-        if 'HEROKU' in os.environ:
-            import urlparse, psycopg2
-            urlparse.uses_netloc.append('postgres')
-            url = urlparse.urlparse(os.environ["postgres://eqvhegcr:uOr1RvzMuCvjqpnSPqvadvOnBwZP5oNd@motty.db.elephantsql.com:5432/eqvhegcr"])
-            db = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
-        else:
-            database = PostgresqlDatabase('eqvhegcr',user='eqvhegcr',password='uOr1RvzMuCvjqpnSPqvadvOnBwZP5oNd',host='motty.db.elephantsql.com', port=5432)
+        import urlparse, psycopg2
+        urlparse.uses_netloc.append('postgres')
+        url = urlparse.urlparse(os.environ["postgres://eqvhegcr:uOr1RvzMuCvjqpnSPqvadvOnBwZP5oNd@motty.db.elephantsql.com:5432/eqvhegcr"])
+        database = PostgresqlDatabase(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port)
+
